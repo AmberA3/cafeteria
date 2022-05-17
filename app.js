1751936c291c6af47a5fb97ebbf4e953978c4de9
Warren Feurtado <wolfefeurtado@gmail.com>
2022-05-17 01:59:25 -0500

    update

--- app.js
+++ app.js
@@ -27,7 +27,6 @@ app.use('/users', usersRouter);
 app.use('/meals', mealsRouter);
 app.use('/admin', adminRouter);
 
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
