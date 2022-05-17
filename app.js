e2b72aa44b9ad06388fe8479a53d492da5170dae
tyndalesanjay <60053448+tyndalesanjay@users.noreply.github.com>
2022-05-17 01:57:20 -0500

    Updated Login

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var mealsRouter = require('./routes/meals');
 var adminRouter = require('./routes/admin');
+var loginRouter = require('./routes/login');
 
 var app = express();
 
@@ -26,6 +27,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/meals', mealsRouter);
 app.use('/admin', adminRouter);
+app.use('/login', loginRouter);
 
 
 // catch 404 and forward to error handler
---
+++ routes/login.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+var conn = require('../lib/db')
+
+/* GET home page. */
+router.get('/', (req, res) => {
+  res.render('login', { title: 'Lunch Ordered'});
+});
+
+module.exports = router;
